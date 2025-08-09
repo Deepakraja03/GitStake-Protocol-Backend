a9d1a829b989759a0a8b32fc91fe014572f88fc0
manicdon7 <manicdon7@gmail.com>
2025-08-10 02:36:24 +0530

    url added

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const cronRoutes = require('./routes/cronRoutes');
 
 // Global middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors("https://git-stake-protocol-frontend.vercel.app/"));
 app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
